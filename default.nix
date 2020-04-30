2207d10933993f3a9437936bfd16e8d916b18808
Dave Syer <dsyer@pivotal.io>
2020-04-30 13:25:10 +0000

    Something that works

--- default.nix
+++ default.nix
@@ -1,26 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 
-let
-
-  pack = stdenv.mkDerivation {
-      name = "pack";
-      src = fetchurl {
-          # nix-prefetch-url this URL to find the hash value
-          url = "https://github.com/buildpacks/pack/releases/download/v0.9.0/pack-v0.9.0-linux.tgz";
-          sha256 = "1admg8rasqg5m4dg3j5xad92pn0f6a55hq2f7i3vl0n9g8b5hna7";
-      };
-      phases = ["installPhase" "patchPhase"];
-      installPhase = ''
-        mkdir -p $out/bin
-        cd $out/bin && tar -zxf $src
-      '';
-  };
-
-in buildEnv {
+stdenv.mkDerivation {
   name = "pack";
-  paths = [
-      pack
-  ];
+  src = fetchurl {
+    # nix-prefetch-url this URL to find the hash value
+    url =
+      "https://github.com/buildpacks/pack/releases/download/v0.9.0/pack-v0.9.0-linux.tgz";
+    sha256 = "1admg8rasqg5m4dg3j5xad92pn0f6a55hq2f7i3vl0n9g8b5hna7";
+  };
+  phases = [ "installPhase" "patchPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cd $out/bin && tar -zxf $src
+  '';
 }
